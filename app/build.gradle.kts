0810fb7ef4e3e8172d28e9a263f3761dd0535f26
AviSoni <aryaveer.2327csit114@kiet.edu>
2024-09-19 10:52:44 +0530

    profile menu added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.database)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
